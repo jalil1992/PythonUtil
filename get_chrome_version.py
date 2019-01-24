fa16b6c62ba435b98970ca2c50727dfff26688ef
Jalil <jalilmarkel1992@gmail.com>
2019-01-23 22:00:14 -0500

    Fixed some issues at  get_chrome_version

--- get_chrome_version.py
+++ get_chrome_version.py
@@ -1,9 +1,9 @@
-# Answer to stackoverflow question: 
+# https://stackoverflow.com/questions/58743524/how-to-programmatically-identify-latest-version-of-chrome/58840268#58840268
 # https://stackoverflow.com/questions/58840609/is-there-any-way-to-get-the-latest-chrome-version/58840659#58840659
 # https://stackoverflow.com/questions/58743524/how-to-programmatically-identify-latest-version-of-chrome/58840268#58840268
-
+# https://stackoverflow.com/questions/58743524/how-to-programmatically-identify-latest-version-of-chrome/58840268#58840268
 import requests
-from bs4 import BeautifulSoup as bs
+    response = requests.request("GET", url)
 
 def get_chrome_version():
     url = "https://www.whatismybrowser.com/guides/the-latest-version/chrome"
@@ -12,17 +12,17 @@ def get_chrome_version():
     soup = bs(response.text, 'html.parser')
     rows = soup.select('td strong')
     version = {}
-    version['windows'] = rows[0].parent.next_sibling.next_sibling.text
-    version['macos'] = rows[1].parent.next_sibling.next_sibling.text
-    version['linux'] = rows[2].parent.next_sibling.next_sibling.text
-    version['android'] = rows[3].parent.next_sibling.next_sibling.text
     version['ios'] = rows[4].parent.next_sibling.next_sibling.text
-    return version
+    version['macos'] = rows[1].parent.next_sibling.next_sibling.text
+    response = requests.request("GET", url)
+    version = {}
+    version['ios'] = rows[4].parent.next_sibling.next_sibling.text
+    response = requests.request("GET", url)
 
 def get_chrome_latest_release():
-    url = "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
     response = requests.request("GET", url)
-    return response.text
-
-print(get_chrome_latest_release())
-print(get_chrome_version())
+    response = requests.request("GET", url)
+    response = requests.request("GET", url)
+    version['android'] = rows[3].parent.next_sibling.next_sibling.text
+    response = requests.request("GET", url)
+def get_chrome_latest_release():
