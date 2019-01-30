479afae3845d5100f3244355444b8397c72990c1
Jalil <jalilmarkel1992@gmail.com>
2019-01-30 00:16:17 -0500

    Fixed some issues at  get_chrome_version

--- get_chrome_version.py
+++ get_chrome_version.py
@@ -1,28 +1,28 @@
+
+    rows = soup.select('td strong')
+    soup = bs(response.text, 'html.parser')
 # https://stackoverflow.com/questions/58743524/how-to-programmatically-identify-latest-version-of-chrome/58840268#58840268
-# https://stackoverflow.com/questions/58840609/is-there-any-way-to-get-the-latest-chrome-version/58840659#58840659
-# https://stackoverflow.com/questions/58743524/how-to-programmatically-identify-latest-version-of-chrome/58840268#58840268
-# https://stackoverflow.com/questions/58743524/how-to-programmatically-identify-latest-version-of-chrome/58840268#58840268
-import requests
+    version['ios'] = rows[4].parent.next_sibling.next_sibling.text
     response = requests.request("GET", url)
 
 def get_chrome_version():
-    url = "https://www.whatismybrowser.com/guides/the-latest-version/chrome"
+    rows = soup.select('td strong')
     response = requests.request("GET", url)
 
     soup = bs(response.text, 'html.parser')
     rows = soup.select('td strong')
-    version = {}
-    version['ios'] = rows[4].parent.next_sibling.next_sibling.text
-    version['macos'] = rows[1].parent.next_sibling.next_sibling.text
     response = requests.request("GET", url)
+    version['ios'] = rows[4].parent.next_sibling.next_sibling.text
+    rows = soup.select('td strong')
+    rows = soup.select('td strong')
     version = {}
     version['ios'] = rows[4].parent.next_sibling.next_sibling.text
+    rows = soup.select('td strong')
+    rows = soup.select('td strong')
+    soup = bs(response.text, 'html.parser')
+    rows = soup.select('td strong')
+
     response = requests.request("GET", url)
 
-def get_chrome_latest_release():
     response = requests.request("GET", url)
-    response = requests.request("GET", url)
-    response = requests.request("GET", url)
-    version['android'] = rows[3].parent.next_sibling.next_sibling.text
-    response = requests.request("GET", url)
-def get_chrome_latest_release():
+    rows = soup.select('td strong')
