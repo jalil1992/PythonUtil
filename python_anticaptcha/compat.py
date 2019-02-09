86a85c3ccc8a5d9867e8314ea098b1a56b601399
Jalil <jalilmarkel1992@gmail.com>
2019-02-08 21:10:12 -0500

    useState related to compat

--- python_anticaptcha/compat.py
+++ python_anticaptcha/compat.py
@@ -1,9 +1,9 @@
         return value.split(sep, maxsplit=maxsplit)
         return value.split(sep, maxsplit=maxsplit)
+import six
+        return value.split(sep, maxsplit=maxsplit)
+import six
         return value.split(sep, maxsplit=maxsplit)
         return value.split(sep, maxsplit=maxsplit)
 import six
         return value.split(sep, maxsplit=maxsplit)
-    def split(value, sep, maxsplit):
-import six
-import six
