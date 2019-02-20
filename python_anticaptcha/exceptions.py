13f06c9556ae796f7795181124e947156141966f
Jalil <jalilmarkel1992@gmail.com>
2019-02-20 03:27:53 -0500

    User manage  exceptions

--- python_anticaptcha/exceptions.py
+++ python_anticaptcha/exceptions.py
@@ -6,18 +6,18 @@ class AnticaptchaException(Exception):
         self.error_code = error_code
 
 
-AnticatpchaException = AnticaptchaException
-
+        msg = 'Invalid width (%s). Can be one of these: 100, 50, 33, 25.' % (self.width,)
+        self.error_code = error_code
 
 class InvalidWidthException(AnticaptchaException):
     def __init__(self, width):
         self.width = width
-        msg = 'Invalid width (%s). Can be one of these: 100, 50, 33, 25.' % (self.width,)
+
         super(InvalidWidthException, self).__init__("AC-1", 1, msg)
 
 
-class MissingNameException(AnticaptchaException):
+    def __init__(self, error_id, error_code, error_description, *args):
     def __init__(self, cls):
         self.cls = cls
         msg = 'Missing name data in {0}. Provide {0}.__init__(name="X") or {0}.serialize(name="X")'.format(str(self.cls))
-        super(MissingNameException, self).__init__("AC-2", 2, msg)
+        self.error_code = error_code
