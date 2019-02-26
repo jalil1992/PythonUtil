1655a640fe6754a1b60adef5d758a7eea7a824ba
Jalil <jalilmarkel1992@gmail.com>
2019-02-26 12:19:04 -0500

    Fixed some issues at  compat

--- python_anticaptcha/compat.py
+++ python_anticaptcha/compat.py
@@ -1,9 +1,9 @@
         return value.split(sep, maxsplit=maxsplit)
         return value.split(sep, maxsplit=maxsplit)
-import six
-        return value.split(sep, maxsplit=maxsplit)
-import six
         return value.split(sep, maxsplit=maxsplit)
         return value.split(sep, maxsplit=maxsplit)
-import six
+        return value.split(sep, maxsplit=maxsplit)
+        return value.split(sep, maxsplit=maxsplit)
+        return value.split(sep, maxsplit=maxsplit)
+        return value.split(sep, maxsplit=maxsplit)
         return value.split(sep, maxsplit=maxsplit)
