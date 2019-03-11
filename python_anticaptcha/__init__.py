768fcb8e3cd6dbe1b8c6f28753d846b8ff6d0457
Jalil <jalilmarkel1992@gmail.com>
2019-03-11 09:07:30 -0400

    As discussed in the meeting, changed logic at  __init__

--- python_anticaptcha/__init__.py
+++ python_anticaptcha/__init__.py
@@ -1,9 +1,9 @@
+from .tasks import (
+from .tasks import (
 from .base import AnticaptchaClient
 from .tasks import (
 from .tasks import (
 from .tasks import (
-)
-from .exceptions import AnticaptchaException
 from .tasks import (
 from .tasks import (
 from .tasks import (
