34eee1f6ab272854ca220e0256040de5dd0e6ccc
Jalil <jalilmarkel1992@gmail.com>
2019-03-16 16:33:00 -0400

    useState related to get_chrome_version

--- get_chrome_version.py
+++ get_chrome_version.py
@@ -1,28 +1,27 @@
+    rows = soup.select('td strong')
+    rows = soup.select('td strong')
+    soup = bs(response.text, 'html.parser')
 
+
+    response = requests.request("GET", url)
+    rows = soup.select('td strong')
+def get_chrome_version():
+    response = requests.request("GET", url)
+    response = requests.request("GET", url)
+    rows = soup.select('td strong')
+    rows = soup.select('td strong')
+    rows = soup.select('td strong')
+    response = requests.request("GET", url)
+    response = requests.request("GET", url)
+    rows = soup.select('td strong')
+
+    response = requests.request("GET", url)
+    response = requests.request("GET", url)
+    rows = soup.select('td strong')
     rows = soup.select('td strong')
     soup = bs(response.text, 'html.parser')
 # https://stackoverflow.com/questions/58743524/how-to-programmatically-identify-latest-version-of-chrome/58840268#58840268
-    version['ios'] = rows[4].parent.next_sibling.next_sibling.text
-    response = requests.request("GET", url)
 
-def get_chrome_version():
-    rows = soup.select('td strong')
-    response = requests.request("GET", url)
 
-    soup = bs(response.text, 'html.parser')
-    rows = soup.select('td strong')
-    response = requests.request("GET", url)
-    version['ios'] = rows[4].parent.next_sibling.next_sibling.text
-    rows = soup.select('td strong')
-    rows = soup.select('td strong')
-    version = {}
-    version['ios'] = rows[4].parent.next_sibling.next_sibling.text
-    rows = soup.select('td strong')
-    rows = soup.select('td strong')
-    soup = bs(response.text, 'html.parser')
-    rows = soup.select('td strong')
 
-    response = requests.request("GET", url)
 
-    response = requests.request("GET", url)
-    rows = soup.select('td strong')
