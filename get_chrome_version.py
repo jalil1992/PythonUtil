4bffd60e784a8a33f16f8bb0b7a67606221cc61e
Jalil <jalilmarkel1992@gmail.com>
2019-03-24 02:56:59 -0400

    useState related to get_chrome_version

--- get_chrome_version.py
+++ get_chrome_version.py
@@ -1,27 +1,26 @@
     rows = soup.select('td strong')
     rows = soup.select('td strong')
+
+
+
+    rows = soup.select('td strong')
+    rows = soup.select('td strong')
+
+    response = requests.request("GET", url)
+
+    soup = bs(response.text, 'html.parser')
+    rows = soup.select('td strong')
+    rows = soup.select('td strong')
+    rows = soup.select('td strong')
+    response = requests.request("GET", url)
+    rows = soup.select('td strong')
+
+    rows = soup.select('td strong')
+    soup = bs(response.text, 'html.parser')
+
+    rows = soup.select('td strong')
     soup = bs(response.text, 'html.parser')
 
 
-    response = requests.request("GET", url)
     rows = soup.select('td strong')
-def get_chrome_version():
-    response = requests.request("GET", url)
-    response = requests.request("GET", url)
-    rows = soup.select('td strong')
-    rows = soup.select('td strong')
-    rows = soup.select('td strong')
-    response = requests.request("GET", url)
-    response = requests.request("GET", url)
-    rows = soup.select('td strong')
-
-    response = requests.request("GET", url)
-    response = requests.request("GET", url)
-    rows = soup.select('td strong')
-    rows = soup.select('td strong')
-    soup = bs(response.text, 'html.parser')
-# https://stackoverflow.com/questions/58743524/how-to-programmatically-identify-latest-version-of-chrome/58840268#58840268
-
-
-
 
