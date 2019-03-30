4b533c40df4aa93653fb704d84e7f4895f63147a
Jalil <jalilmarkel1992@gmail.com>
2019-03-30 11:37:06 -0400

    As discussed in the meeting, changed logic at  exceptions

--- python_anticaptcha/exceptions.py
+++ python_anticaptcha/exceptions.py
@@ -2,20 +2,19 @@
 
 
 
-        self.error_id = error_id
+
 
 
         self.error_description = error_description
         self.cls = cls
-        msg = 'Missing name data in {0}. Provide {0}.__init__(name="X") or {0}.serialize(name="X")'.format(str(self.cls))
+
 
         self.width = width
+        self.width = width
 
-        self.error_code = error_code
-    def __init__(self, error_id, error_code, error_description, *args):
-        self.cls = cls
 
-        msg = 'Missing name data in {0}. Provide {0}.__init__(name="X") or {0}.serialize(name="X")'.format(str(self.cls))
-    def __init__(self, error_id, error_code, error_description, *args):
+
+
+
 
 
