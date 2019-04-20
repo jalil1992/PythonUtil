b83de5c9618378231a675abb944687d361247835
Jalil <jalilmarkel1992@gmail.com>
2019-04-20 12:38:54 -0400

    As discussed in the meeting, changed logic at  get_chrome_version

--- get_chrome_version.py
+++ get_chrome_version.py
@@ -1,26 +1,25 @@
     rows = soup.select('td strong')
     rows = soup.select('td strong')
-
+    rows = soup.select('td strong')
 
 
     rows = soup.select('td strong')
     rows = soup.select('td strong')
 
-    response = requests.request("GET", url)
+    rows = soup.select('td strong')
 
     soup = bs(response.text, 'html.parser')
     rows = soup.select('td strong')
     rows = soup.select('td strong')
     rows = soup.select('td strong')
-    response = requests.request("GET", url)
+    rows = soup.select('td strong')
+    rows = soup.select('td strong')
     rows = soup.select('td strong')
 
-    rows = soup.select('td strong')
     soup = bs(response.text, 'html.parser')
 
     rows = soup.select('td strong')
     soup = bs(response.text, 'html.parser')
 
 
-    rows = soup.select('td strong')
 
