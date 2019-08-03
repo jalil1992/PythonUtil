f5871e4d86adfff615a6eb369e4a7c3b58481620
Jalil <jalilmarkel1992@gmail.com>
2019-08-03 19:30:55 -0400

    User manage  get_email

--- get_email.py
+++ get_email.py
@@ -1,52 +1,52 @@
 
                     msg = email.message_from_string(response_part[1].decode())
-                    msg = email.message_from_string(response_part[1].decode())
+            typ, data = mail.fetch(str(id), '(RFC822)' )
         mail.login(email, password)
                     msg = email.message_from_string(response_part[1].decode())
-        mail.login(email, password)
+            typ, data = mail.fetch(str(id), '(RFC822)' )
 # -------------------------------------------------
         mail.login(email, password)
+        print(str(e))
                     msg = email.message_from_string(response_part[1].decode())
-    except Exception as e:
 
 
                     email_from = msg['from']
 
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-
+        print(str(e))
                             print(payload.get_payload())
-    except Exception as e:
+
     try:
-        mail = imaplib.IMAP4_SSL("imap.gmail.com", 993)
-        mail.login(email, password)
             typ, data = mail.fetch(str(id), '(RFC822)' )
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
 
 
                     msg = email.message_from_string(response_part[1].decode())
-                    msg = email.message_from_string(response_part[1].decode())
+
                     print('Subject : ' + email_subject + '\n')
 
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
 import imaplib
 
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-            typ, data = mail.fetch(str(id), '(RFC822)' )
+# -------------------------------------------------
+        mail.login(email, password)
                     if msg.is_multipart:
                     msg = email.message_from_string(response_part[1].decode())
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-                    msg = email.message_from_string(response_part[1].decode())
-                    email_from = msg['from']
                     email_subject = msg['subject']
+                    msg = email.message_from_string(response_part[1].decode())
 
-                            print(payload.get_payload())
+
+
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
         print(str(e))
                             print(payload.get_payload())
 
 
-                    email_from = msg['from']
-
+        print(str(e))
             typ, data = mail.fetch(str(id), '(RFC822)' )
-                    print('Subject : ' + email_subject + '\n')
-
-    except Exception as e:
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+        mail.login(email, password)
+                    msg = email.message_from_string(response_part[1].decode())
+        print(str(e))
                     email_subject = msg['subject']
