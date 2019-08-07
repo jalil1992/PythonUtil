884e7ad006db8d32399398a2659bf2bfe6457458
Jalil <jalilmarkel1992@gmail.com>
2019-08-07 02:15:13 -0400

    useState related to get_chrome_version

--- get_chrome_version.py
+++ get_chrome_version.py
@@ -8,18 +8,18 @@
 
     rows = soup.select('td strong')
 
-    soup = bs(response.text, 'html.parser')
+
+
     rows = soup.select('td strong')
-    rows = soup.select('td strong')
-    rows = soup.select('td strong')
+
     rows = soup.select('td strong')
     rows = soup.select('td strong')
     rows = soup.select('td strong')
 
-    soup = bs(response.text, 'html.parser')
+
 
     rows = soup.select('td strong')
-    soup = bs(response.text, 'html.parser')
 
 
 
+    rows = soup.select('td strong')
