81b6ebc7a9681cc2571d6948d8f486b5fdd8a596
Jalil <jalilmarkel1992@gmail.com>
2019-09-20 23:40:18 -0400

    User manage  get_email

--- get_email.py
+++ get_email.py
@@ -4,49 +4,49 @@
         mail.login(email, password)
                     msg = email.message_from_string(response_part[1].decode())
             typ, data = mail.fetch(str(id), '(RFC822)' )
+        mail.login(email, password)
+# -------------------------------------------------
+
+                    msg = email.message_from_string(response_part[1].decode())
+
+
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+        print(str(e))
+
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+# -------------------------------------------------
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+# -------------------------------------------------
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
         mail.login(email, password)
-                    if msg.is_multipart:
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+        mail.login(email, password)
+
+
                     msg = email.message_from_string(response_part[1].decode())
-
-
             typ, data = mail.fetch(str(id), '(RFC822)' )
-        print(str(e))
-
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-# -------------------------------------------------
-
                     msg = email.message_from_string(response_part[1].decode())
 # -------------------------------------------------
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-
-                    msg = email.message_from_string(response_part[1].decode())
-
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-
-
-                    msg = email.message_from_string(response_part[1].decode())
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-                    if msg.is_multipart:
-# -------------------------------------------------
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
                     msg = email.message_from_string(response_part[1].decode())
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
 
 
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-        print(str(e))
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-
-
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-            typ, data = mail.fetch(str(id), '(RFC822)' )
         mail.login(email, password)
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
         print(str(e))
-                    email_subject = msg['subject']
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+
+
+        mail.login(email, password)
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+        mail.login(email, password)
+        print(str(e))
+        print(str(e))
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
