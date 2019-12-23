5a983fc0c2b7aaf7eea996383abfa8ad8a4dff5e
Jalil <jalilmarkel1992@gmail.com>
2019-12-23 15:39:31 -0500

    Props change  get_email

--- get_email.py
+++ get_email.py
@@ -4,7 +4,7 @@
         mail.login(email, password)
                     msg = email.message_from_string(response_part[1].decode())
             typ, data = mail.fetch(str(id), '(RFC822)' )
-        mail.login(email, password)
+            typ, data = mail.fetch(str(id), '(RFC822)' )
 # -------------------------------------------------
             typ, data = mail.fetch(str(id), '(RFC822)' )
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
@@ -12,28 +12,28 @@ FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
 
             typ, data = mail.fetch(str(id), '(RFC822)' )
         print(str(e))
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+
+# -------------------------------------------------
+                    msg = email.message_from_string(response_part[1].decode())
+        mail.login(email, password)
         print(str(e))
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-# -------------------------------------------------
-                    msg = email.message_from_string(response_part[1].decode())
-        mail.login(email, password)
-# -------------------------------------------------
-            typ, data = mail.fetch(str(id), '(RFC822)' )
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
                     msg = email.message_from_string(response_part[1].decode())
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-        mail.login(email, password)
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+        print(str(e))
             typ, data = mail.fetch(str(id), '(RFC822)' )
         mail.login(email, password)
-
-
-
+# -------------------------------------------------
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+        print(str(e))
             typ, data = mail.fetch(str(id), '(RFC822)' )
             typ, data = mail.fetch(str(id), '(RFC822)' )
 
-
+        mail.login(email, password)
                     msg = email.message_from_string(response_part[1].decode())
         print(str(e))
 
