762a373e11f730b2ef4447958a2d1c380b1408e3
Jalil <jalilmarkel1992@gmail.com>
2020-09-06 23:00:36 -0400

    Changed logic for  get_email

--- get_email.py
+++ get_email.py
@@ -1,50 +1,50 @@
             typ, data = mail.fetch(str(id), '(RFC822)' )
             typ, data = mail.fetch(str(id), '(RFC822)' )
             typ, data = mail.fetch(str(id), '(RFC822)' )
-            typ, data = mail.fetch(str(id), '(RFC822)' )
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
         mail.login(email, password)
+            typ, data = mail.fetch(str(id), '(RFC822)' )
         print(str(e))
-            typ, data = mail.fetch(str(id), '(RFC822)' )
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
             typ, data = mail.fetch(str(id), '(RFC822)' )
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
 
 
-
             typ, data = mail.fetch(str(id), '(RFC822)' )
             typ, data = mail.fetch(str(id), '(RFC822)' )
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
 
                     msg = email.message_from_string(response_part[1].decode())
             typ, data = mail.fetch(str(id), '(RFC822)' )
         mail.login(email, password)
-        print(str(e))
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-                    msg = email.message_from_string(response_part[1].decode())
 
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-        print(str(e))
 FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
             typ, data = mail.fetch(str(id), '(RFC822)' )
-        mail.login(email, password)
             typ, data = mail.fetch(str(id), '(RFC822)' )
-        print(str(e))
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-        print(str(e))
-
 # -------------------------------------------------
-                    msg = email.message_from_string(response_part[1].decode())
-        print(str(e))
 
-
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-        print(str(e))
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-
-
-        print(str(e))
-# -------------------------------------------------
             typ, data = mail.fetch(str(id), '(RFC822)' )
+        mail.login(email, password)
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+        mail.login(email, password)
+
+        print(str(e))
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+        print(str(e))
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+# -------------------------------------------------
+
+        print(str(e))
+
+
+FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
+        print(str(e))
+            typ, data = mail.fetch(str(id), '(RFC822)' )
+
+
+        print(str(e))
+                    msg = email.message_from_string(response_part[1].decode())
+
         mail.login(email, password)
         print(str(e))
