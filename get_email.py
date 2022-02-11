ac24b0c0a62211723619d12213f9345f787d2dee
Jalil Markel <98934018+jalil1992@users.noreply.github.com>
2022-02-11 09:29:50 -0500

    Update get_email.py

--- get_email.py
+++ get_email.py
@@ -1,50 +1,59 @@
-
-        mail.login(email, password)
-
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-        mail.login(email, password)
-
-        print(str(e))
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-        print(str(e))
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-
-                    msg = email.message_from_string(response_part[1].decode())
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-
-                    msg = email.message_from_string(response_part[1].decode())
-        mail.login(email, password)
-        mail.login(email, password)
+import smtplib
+import time
+import imaplib
+import email
 
 # -------------------------------------------------
-        mail.login(email, password)
-        mail.login(email, password)
-# -------------------------------------------------
-        mail.login(email, password)
+# Utility to read email from Gmail Using Python
+# ------------------------------------------------
+def read_email(email, password):
+    if email.endswith("@gmail.com"):
+        return read_email_from_gmail(email, password)
+    else:
+        raise NotImplementedError
+
+FROM_EMAIL  = "#####"
+FROM_PWD    = "#####"
+
+def read_email_from_gmail(_email, _password):
+    try:
+        mail = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+        mail.login(_email, _password)
+        print(mail.select('"[Gmail]/All Mail"'))
+        rv, mailboxes = mail.list()
+        
+
+        # type, data = mail.search(None, 'HEADER Subject "RamCity Pty Ltd Order Receipt"')
+        type, data = mail.search(None, r'X-GM-RAW "subject:(RamCity Pty Ltd Order Receipt#) has:attachment"')
+        # type, data = mail.search(None, 'ALL')
+        
+        mail_ids = data[0]
+
+        id_list = mail_ids.split()   
+        print(len(id_list))
+        first_email_id = int(id_list[0])
+        latest_email_id = int(id_list[-1])
+
+        count = 0
+        for id in range(latest_email_id,first_email_id, -1):
             typ, data = mail.fetch(str(id), '(RFC822)' )
-        mail.login(email, password)
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-        mail.login(email, password)
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
 
-
-
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-                    msg = email.message_from_string(response_part[1].decode())
-                    msg = email.message_from_string(response_part[1].decode())
-        print(str(e))
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-
-FROM_EMAIL  = "XXXXXXXXXXXXXX@gmail.com"
-        print(str(e))
-        mail.login(email, password)
-
-            typ, data = mail.fetch(str(id), '(RFC822)' )
-        print(str(e))
+            for response_part in data:
+                if isinstance(response_part, tuple):
                     msg = email.message_from_string(response_part[1].decode())
 
-        mail.login(email, password)
+                    email_subject = msg['Subject']
+                    email_from = msg['from']
+                    
+                    # if msg.is_multipart:
+                    #     for payload in msg.get_payload():
+                    #         print(payload.get_payload())
+                    # else:
+                    #     print(msg.get_payload())
+                        
+                    # print('From : ' + email_from + '\n')
+                    print('Subject : ',email_subject)
+    except Exception as e:
         print(str(e))
+
+read_email_from_gmail(FROM_EMAIL, FROM_PWD)
